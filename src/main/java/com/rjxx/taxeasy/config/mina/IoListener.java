09ce0ad7e089a982a17b90730bb7744f25e26e49
kangzhongxu <kangzhongxu@datarj.com>
2018-06-22 11:05:31 +0800

    日志输出

--- src/main/java/com/rjxx/taxeasy/config/mina/IoListener.java
+++ src/main/java/com/rjxx/taxeasy/config/mina/IoListener.java
@@ -4,6 +4,8 @@ import org.apache.mina.core.service.IoService;
 import org.apache.mina.core.service.IoServiceListener;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @ClassName IoListener
@@ -13,33 +15,36 @@ import org.apache.mina.core.session.IoSession;
  * @Version 1.0
  **/
 public class IoListener implements IoServiceListener {
+
+    private static Logger logger = LoggerFactory.getLogger(ServerHandler.class);
     @Override
     public void serviceActivated(IoService service) throws Exception {
 
+        logger.info("#######IoListener监听:serviceActivated####");
     }
 
     @Override
     public void serviceIdle(IoService service, IdleStatus idleStatus) throws Exception {
-
+        logger.info("#######IoListener监听:serviceIdle####");
     }
 
     @Override
     public void serviceDeactivated(IoService service) throws Exception {
-
+        logger.info("#######IoListener监听:serviceDeactivated####");
     }
 
     @Override
     public void sessionCreated(IoSession session) throws Exception {
-
+        logger.info("#######IoListener监听:sessionCreated####");
     }
 
     @Override
     public void sessionClosed(IoSession session) throws Exception {
-
+        logger.info("#######IoListener监听:sessionClosed####");
     }
 
     @Override
     public void sessionDestroyed(IoSession session) throws Exception {
-
+        logger.info("#######IoListener监听:sessionDestroyed####");
     }
 }
